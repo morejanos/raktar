21535d80a08998ce556db1a193ec9f8e4282d5a8
morejanos <janos.more@gmail.com>
2015-04-12 12:25:06 +0200

    Componenttype translate to Hun

--- raktar/app/models/component.rb
+++ raktar/app/models/component.rb
@@ -5,6 +5,7 @@ class Component < ActiveRecord::Base
     belongs_to :user
 
     validates :name, presence: true
+    validates :componenttype, presence: true
     validates :user_id, presence: true
     validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
    
