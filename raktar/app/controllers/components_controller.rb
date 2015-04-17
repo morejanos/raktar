f5ba1252fc6f8808b27dbf7e893c45d32e4c9de1
morejanos <janos.more@gmail.com>
2015-04-17 08:18:10 +0200

    Add search session to component controller and view

--- raktar/app/controllers/components_controller.rb
+++ raktar/app/controllers/components_controller.rb
@@ -5,8 +5,10 @@ class ComponentsController < ApplicationController
   # GET /components
   # GET /components.json
   def index
-    type = params[:componenttype] || ""
-    @components = Component.search(params[:search], type)
+    update_search_session(params[:search])
+    update_componenttype_session(params[:componenttype])
+    
+    @components = Component.search(session[:search], session[:componenttype])
   end
 
   # GET /components/1
@@ -70,4 +72,25 @@ class ComponentsController < ApplicationController
     def component_params
       params.require(:component).permit(:name, :partnumber, :inventory, :power, :voltage, :current, :resistance, :dimension, :temperature, :capacity, :location, :comment, :comment, :criticalNrOfPieces, :packaging_id, :user_id, :componenttype_id, :manufacturer_id)
     end
+
+    def update_search_session(search)
+      if session[:search].nil? && !search.empty? then
+        session[:search] = search
+      elsif session[:search].nil? then
+        session[:search] = search
+      elsif session[:search] != search then
+        session[:search] = search
+      end
+    end
+    
+    def update_componenttype_session(componenttype)
+      if session[:componenttype].nil? && !componenttype.nil? then
+        session[:componenttype] = componenttype
+      elsif session[:componenttype].nil? || componenttype.nil? then
+        session[:componenttype] = ""
+      elsif session[:componenttype] != componenttype then
+        session[:componenttype] = componenttype
+      end
+    end
+
 end
