bf0e29df36eb4da3a90c1f8a8f65130fa275b777
mmvill Raktár <mmvillraktar@localhost.localdomain>
2016-06-10 15:00:32 +0200

    Now really fix Homepage link

--- raktar/app/helpers/application_helper.rb
+++ raktar/app/helpers/application_helper.rb
@@ -47,7 +47,7 @@ module ApplicationHelper
   def site_url
     if Rails.env.production?
     # Place your production URL in the quotes below
-    "http:/192.168.1.119:3000"
+    "http://192.168.1.119:3000"
     else
     # Our dev & test URL
     "http://localhost:3000"
