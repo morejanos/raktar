4194ceb5c9f0177edb3abf5ea9f6b9584609702b
mmvill Raktár <mmvillraktar@localhost.localdomain>
2016-07-01 11:46:37 +0200

    Email Multiple recipients bug fixed

--- raktar/app/mailers/usermailer.rb
+++ raktar/app/mailers/usermailer.rb
@@ -2,9 +2,14 @@ class Usermailer < ApplicationMailer
     def criticalNrOfPieces_email(component)
         @component = component
         p = Permission.find_by name: 'poweruser'
-        
+       
+	recipients = Array.new
+
         p.users.each do |user|
-            mail(to: user.email, subject: "Raktarkeszlet fogyoban")
+       	    recipients.push(user.email)	
         end
+
+        mail(to: recipients , subject: "Raktarkeszlet fogyoban")
+
     end
 end
