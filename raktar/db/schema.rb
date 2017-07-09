f1db7e9256872388a4e3e53e8c2831b89a1187d9
mmvill Raktár <mmvillraktar@localhost.localdomain>
2017-07-09 14:44:29 +0200

    new migration was ran and new schema.rb was modified.
    janos.more@gmail.com

--- raktar/db/schema.rb
+++ raktar/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610194239) do
+ActiveRecord::Schema.define(version: 20170706062746) do
 
   create_table "components", force: :cascade do |t|
     t.string   "name",               limit: 255
@@ -70,6 +70,16 @@ ActiveRecord::Schema.define(version: 20170610194239) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255,   null: false
+    t.text     "data",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "statuses", force: :cascade do |t|
     t.string "name",        limit: 255
